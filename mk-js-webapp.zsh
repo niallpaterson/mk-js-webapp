b7db934e5d3307f7fe8eedc50a8f350e4e9a61f7
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-05 12:53:03 +0100

    add root script

---
+++ mk-js-webapp.zsh
@@ -0,0 +1,69 @@
+#!/usr/bin/env zsh
+
+set -k 
+
+read -- 'APPNAME?Enter name of app: (my-app) '
+APPNAME=${APPNAME:-'my-app'}
+
+mkdir ${APPNAME}
+cd ${APPNAME}
+
+git init
+
+touch '.gitignore'
+. ../templates/gitignore.zsh
+
+touch 'README.md'
+
+touch 'package.json'
+. ../templates/package.json.zsh
+
+mkdir html
+cd html
+touch 'index.html'
+. ../../templates/html.zsh
+cd ..
+
+mkdir js
+cd js
+touch 'app.js'
+touch 'app.test.js'
+cd ..
+. ../packages/jest.zsh
+
+mkdir scss
+cd ./scss
+. ../../templates/scss.zsh
+cd ..
+
+mkdir .github
+cd ./.github
+touch 'settings.yml'
+. ../../templates/labels.zsh
+cd ..
+
+touch 'babel.config.js'
+. ../templates/babel-config.zsh
+
+touch '.eslintrc.js'
+. ../templates/eslint-config.zsh
+. ../packages/eslint.zsh
+
+mkdir 'buildTests'
+cd 'buildTests'
+touch testJest.js
+. ../../templates/test-jest.zsh
+touch testJest.test.js
+. ../../templates/test-jest-test.zsh
+touch testLint.js
+. ../../templates/test-lint.zsh
+
+yarn test buildTests/testJest.test.js
+
+cd ../../..
+pwd
+mv mk-js-webapp/${APPNAME} $(pwd)
+
+echo '👍 Good to go'
+
+exit
