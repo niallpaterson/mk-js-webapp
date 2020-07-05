af1c380c25106c9ca962a8c3f015701b828136a8
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-05 12:51:39 +0100

    add babel config template

---
+++ templates/babel-config.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+
+echo '// babel.config.js
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+  ],
+};' > babel.config.js
