5e6279e65a83ab0ad37c90a49c172ae858b2a0a8
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-07-05 12:52:43 +0100

    add eslint package installers

---
+++ packages/eslint.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+set -k
+
+yarn add --dev eslint@^7.2.0 
+yarn add --dev eslint-plugin-import@^2.21.2
+yarn add --dev eslint-config-airbnb-base@^14.2.0 
+yarn add --dev object.entries@^1.1.2 
+yarn add --dev object.assign@^4.1.0
